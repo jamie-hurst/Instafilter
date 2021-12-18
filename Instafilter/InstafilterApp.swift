e4724d9a29594aecdfb39096ce546673d8627adc
warmrome <pxpx786@gmail.com>
2021-12-18 11:17:56 -0800

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Jameson Hurst on 12/18/21.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
